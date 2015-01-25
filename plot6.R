6cafdb1a9b93f330d6be47f59e7e5fff9c74c14b
Kevin Field <kevin.field.21@gmail.com>
2015-01-25 14:39:21 -0700

    Done!

--- plot6.R
+++ plot6.R
@@ -12,26 +12,26 @@ NEI <- tbl_df(NEI)                                # convert to dplyr data frame
 SCC <- tbl_df(SCC)
                                                   # get all SCC types that are Motor Vehicles (both Onroad and Nonroad)
 scc <- filter(SCC, grepl("Onroad|Nonroad", SCC$Data.Category, ignore.case=TRUE))
-scc <- scc$SCC                                    # remove all other columns in the df
-scc <- as.character(scc)                          # convert to a character vector
+scc <- scc$SCC 
+scc <- as.character(scc)
 
 bc <- filter(NEI, fips == "24510")                # get Baltimore City data
-bc <- filter(bc, grepl(paste(scc, collapse="|"), bc$SCC)) # filter based on the SCC codes
-bc <- group_by(bc, year)                          # group the data by year
-bc <- summarize(bc, emissions = sum(Emissions))   # sum the emissions values
-bc$year <- as.character(bc$year)                  # convert year values to characters for x axis labels in geom_bar
+bc <- filter(bc, grepl(paste(scc, collapse="|"), bc$SCC)) 
+bc <- group_by(bc, year)
+bc <- summarize(bc, emissions = sum(Emissions))
+bc$year <- as.character(bc$year)
 
 lac <- filter(NEI, fips == "06037")               # get Los Angeles County data
-lac <- filter(lac, grepl(paste(scc, collapse="|"), lac$SCC)) # filter based on the SCC codes
-lac <- group_by(lac, year)                        # group the data by year
-lac <- summarize(lac, emissions = sum(Emissions)) # sum the emissions values
-lac$year <- as.character(lac$year)                # convert year values to characters for x axis labels in geom_bar
+lac <- filter(lac, grepl(paste(scc, collapse="|"), lac$SCC))
+lac <- group_by(lac, year)
+lac <- summarize(lac, emissions = sum(Emissions))
+lac$year <- as.character(lac$year)
 
 bc <- cbind(bc, city = "Baltimore City")          # add city variable
 lac <- cbind(lac, city = "Los Angeles County")
 data <- rbind(bc, lac)                            # combine data from both cities
 
-png("plot6.png", width=640, height=480)           # set the PNG device for plotting
+png("plot6.png", width=640, height=480)
 
 g <- ggplot(data, aes(year, emissions, group = 1)) +
         geom_bar(stat = "identity", fill = "lightskyblue", color = "darkgray") +
@@ -43,6 +43,6 @@ g <- ggplot(data, aes(year, emissions, group = 1)) +
 
 print(g)
 
-dev.off()                                         # close the png device
+dev.off()
 
 # rm(list=ls())
